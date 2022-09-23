9dae0008f93d85eacbeaf5ff91c33c9e69276b11
Ba1yun <102446297+Ba1yun@users.noreply.github.com>
2022-09-23 20:19:53 +0800

    Add files via upload

--- c.c
+++ c.c
@@ -1,60 +1,79 @@
-#include <stdio.h>
-//二分查找
-int main()
-{
-	int arr[] = {1,2,3,3,4,5,6,7,8,9,10,11};
-	int k = 13;
-	int sz = sizeof(arr)/sizeof(arr[0]);
-	int left = 0;
-	int right = sz - 1;
 
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		if (arr[mid] > k)
-		{
-			right = mid - 1;
-		}
-		else if (arr[mid] < k)
-		{
-			left = mid + 1;
-		}
-		else 
-		{
-			printf("找到了，下标是：%d\n", mid);
-			break;
-		}
-
-	}
-	if (left > right)
-		printf("找不到\n");
-
-	return 0;
-}
-
-
-
-
+//二维数组
+//#include<stdio.h>
 //int main()
 //{
-//	int b = 1;
-//	int i = 1;
-//	int n = 0;
-//	int ret = 0;
-//	int j = 1;
-//	printf("请输入n:");
-//	scanf_s("%d\n", &n);
-//	for (j = 1; j < n + 1; j++)
+//	int arr[3][4] = {{1,2,3},{4,5}};
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
 //	{
-//		b = 1;
-//		for (i = 1; i < j + 1; i++)
-//			b = b*i;
-//		ret = ret + b;
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%p ", &arr[i][j]);
+//		}
+//		printf("\n");
 //	}
-//	
-//		
-//	printf("n的阶乘为：%d\n", b);
-//	printf("n的阶乘为：%d\n", ret );
 //
 //	return 0;
 //}
+
+//自定义求字符串长度
+//求长度大小，可以用库函数sizeof()或者strlen()
+//#include<stdio.h>
+
+//没有临时变量
+//int len(char* s)
+//{
+//	while (*s != "\0")
+//	{
+//		s++;
+//	}
+//	return s;
+//}
+//int main()
+//{
+//	char arr[] = "lim";
+//	int ret = len(arr);
+//	printf("长度为：%d", ret);
+//
+//	return 0;
+//}
+//排序
+#include<stdio.h>
+sort(int brr[],int sz)
+{
+	//冒泡趟数
+	//int szz = sizeof(brr) / sizeof(brr[0]);
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - i - 1; j++)
+		{
+			if (brr[j] > brr[j + 1])
+			{
+				int ret = brr[j];
+				brr[j] = brr[j + 1];
+				brr[j + 1] = ret;
+
+			}
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1,0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	sort(arr, sz);
+	for (i = 0; i < sz; i++)
+	{
+		//sort(arr,sz);
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+
+}
